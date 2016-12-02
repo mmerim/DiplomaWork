88ecebbeb6551517a5ed79a0381311f0d89c629a
Meri <manucharyan.m@inbox.ru>
2016-12-02 17:20:02 +0400

    Initial Commit

---
+++ DiplomaWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiplomaWork
+//
+//  Created by Meri on 12/2/16.
+//  Copyright © 2016 Meri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
